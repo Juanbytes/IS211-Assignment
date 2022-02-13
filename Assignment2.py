d6a76b0c98ac8443a75a6f12401b153b77fb31dc
jflor <jflorentino0791@gmail.com>
2022-02-12 22:56:56 -0500

    assignment2.py

--- Assignment2.py
+++ Assignment2.py
@@ -1,13 +1,18 @@
+import urllib.request
 import argparse
 import datetime
 import logging
-import urllib
+
+
+logging.basicConfig(filename="errors.log", level=logging.ERROR)
+assignment2 = logging.getLogger()
 
 def downloadData(url):
     with urllib.request.urlopen(url) as response:
-        data = reponse.read().decode('utf-8')
+        data = response.read().decode('utf-8')
 
     return data
+
 
 def processData(file_content):
     person_data = {}
@@ -16,28 +21,15 @@ def processData(file_content):
         if len(data_info) == 0:
             continue
 
-        identifier, name, birthday = data_info.split(",")
-        if identifier == "id":
-            continue
+    id, name, birthday = data_info.split(",")
+    if id == "id":
 
-        id_int = int(identifier)
-        try:
-            true_birthday = datetime.datetime.strptime(birthday, "%d/%m/%Y")
-            person_data[id_int] = (name, true_birthday)
-        except ValueError as e:
-            print(f"error parsing {birthday}")
-
-    return person_data
-
-    elements = line.split(",")
-    id = int(elements[0])
-    name = elements[1]
+         id_int = int(id)
     try:
-        birthday = datetime.datetime.strptime(elements[2], "%d/%m/%Y")
-    except ValueError:
-        logging.error(f"Error processing line #{i} for ID #{id}")
-
-        person_data[id] = (name, birthday)
+        true_birthday = datetime.datetime.strptime(birthday, "%d/%m/%Y")
+        person_data[id_int] = (name, true_birthday)
+    except ValueError as e:
+        print(f"error parsing {birthday}")
 
     return person_data
 
@@ -48,20 +40,20 @@ def displayPerson(id, personData):
     except KeyError:
         print(f"No user found with that id")
 
-
 def main(url):
     print(f"Running main with URL = {url}...")
-    content = downloadData(url)
-    personData = processData(content)
+    file_content = downloadData(url)
+    print(file_content)
+    personData = processData(file_content)
     while True:
         id = int(input("Enter an ID:"))
-        if id < 0:
-            break
+    if id > 0:
         displayPerson(id, personData)
-        print(personData)
+    print(personData)
 
-if __name__ != "__main__":
+
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--url", help="URL to the datafile", type=str, required=True)
-    args + parser.parse_args()
+    args = parser.parse_args()
     main(args.url)
